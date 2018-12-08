8b2201876d5820662536b7732e69ecd9158b24a7
Sebastian Schiener <lauterach@gmx.at>
2018-12-08 17:48:27 +0100

    first commit, testing

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Weihnachts-Spiele'},
+    {id: 2, name: 'Silvester-Wettkampf'},
+    {id: 3, name: 'Oster-Wettkampf'},
+    {id: 4, name: 'Liga-Meisterschaft'}
+  ]
+;
