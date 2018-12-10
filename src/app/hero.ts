838ee0b2b3c974dacdefd0a94eb1a6a883a6b8fe
Sebastian Schiener <lauterach@gmx.at>
2018-12-10 12:15:15 +0100

    liste

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,11 @@
 export class Hero {
   id: number;
   name: string;
+  sportart: string;
+  liga: string;
+  gebuehr: number;
+  ort: string;
+  startdatum: string;
+  enddatum: string;
+  begegnungen: string;
 }
