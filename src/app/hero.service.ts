4634eff1f2996e8b4ae09519b31b946cc61d329b
Sebastian Schiener <lauterach@gmx.at>
2018-12-11 15:02:09 +0100

    refactor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -24,7 +24,7 @@ export class HeroService {
   getHeroes (): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
-        tap(_ => this.log('fetched heroes')),
+        tap(_ => this.log('fetched competitions')),
         catchError(this.handleError('getHeroes', []))
       );
   }
@@ -47,7 +47,7 @@ export class HeroService {
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
-      tap(_ => this.log(`fetched hero id=${id}`)),
+      tap(_ => this.log(`fetched competition id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
@@ -115,6 +115,6 @@ export class HeroService {
 
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
+    this.messageService.add(`CompetitionService: ${message}`);
   }
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,10 @@ import { Injectable } from '@angular/core';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 1, name: 'Weihnachtsspiele', sportart: 'Fussball' },
+      { id: 2, name: 'Silvesterspiele' },
+      { id: 3, name: 'Osterturnier' },
+      { id: 4, name: 'Meisterschaft' }
     ];
     return {heroes};
   }
